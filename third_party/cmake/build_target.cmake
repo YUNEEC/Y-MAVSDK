0cb4e1f1e645bebda0666744083be1aa7c94f106
Yonghua Zheng <yh.zheng@yuneec.com>
2021-09-13 20:07:41 +0800

    Add ANDROID_ABI and ANDROID_PLATFORM for build consistency between inter-cmake process

--- third_party/cmake/build_target.cmake
+++ third_party/cmake/build_target.cmake
@@ -11,6 +11,25 @@ function(build_target TARGET_NAME)
         set(PLATFORM_ARGUMENT "-A${CMAKE_GENERATOR_PLATFORM}")
     endif()
 
+    if(ANDROID)
+    execute_process(
+        COMMAND ${CMAKE_COMMAND}
+            "-G${CMAKE_GENERATOR}"
+            "${PLATFORM_ARGUMENT}"
+            "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+            "-DANDROID_ABI=${ANDROID_ABI}"
+            "-DANDROID_PLATFORM=${ANDROID_PLATFORM}"
+            "-DCMAKE_TOOLCHAIN_FILE:PATH=${CMAKE_TOOLCHAIN_FILE}"
+            "-DCMAKE_INSTALL_PREFIX:PATH=${TARGET_INSTALL_DIR}"
+            "-DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}"
+            "-DPLATFORM=${PLATFORM}" # for iOS toolchain
+            "-DDEPLOYMENT_TARGET=${DEPLOYMENT_TARGET}" # for iOS toolchain
+            "-DENABLE_STRICT_TRY_COMPILE=${ENABLE_STRICT_TRY_COMPILE}" # for iOS toolchain
+            "${TARGET_SOURCE_DIR}"
+        WORKING_DIRECTORY "${TARGET_BINARY_DIR}"
+        RESULT_VARIABLE CONFIGURE_FAILED
+    )
+    else()
     execute_process(
         COMMAND ${CMAKE_COMMAND}
             "-G${CMAKE_GENERATOR}"
@@ -26,6 +45,7 @@ function(build_target TARGET_NAME)
         WORKING_DIRECTORY "${TARGET_BINARY_DIR}"
         RESULT_VARIABLE CONFIGURE_FAILED
     )
+    endif()
 
     if(${CONFIGURE_FAILED})
         message(FATAL_ERROR "${TARGET_SOURCE_DIR} failed to configure!")
